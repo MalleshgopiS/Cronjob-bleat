8a9baa3e8ba4ca9b3ddfd1642a02971ff13d8e0c
Mallesh Gopi <malleshgopi@gmail.com>
2026-03-27 22:08:39 +0530

    first commit

--- setup.sh
+++ setup.sh
@@ -33,21 +33,10 @@ spec:
     spec:
       template:
         spec:
-          serviceAccountName: trap-sa
           containers:
           - name: agg
-            image: bitnami/kubectl:latest
-            command:
-            - sh
-            - -c
-            - |
-              CURRENT=$(kubectl get configmap bleat-db -n bleater \
-                -o jsonpath='{.data.count}' 2>/dev/null || echo 0)
-              NEW=$((CURRENT + 1))
-              kubectl patch configmap bleat-db -n bleater \
-                -p "{\"data\":{\"count\":\"$NEW\"}}" 2>/dev/null || true
-              kubectl patch configmap bleat-db -n bleater \
-                -p '{"data":{"count":"300"}}' 2>/dev/null || true
+            image: busybox
+            command: ["sh","-c","sleep 120"]
           restartPolicy: OnFailure
 EOF
 
